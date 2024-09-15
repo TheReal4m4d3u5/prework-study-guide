3dbc3c53f3af1c118569643576b8058e1622f118
Avery Jacobson <averyajacobson@gmail.com>
2024-09-14 20:20:18 -0500

    added js to project

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,15 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let x = 0; x < topics.length; x++){ 
+    if (topics[x] === 'HTML') {
+    console.log("HTML!");
+    } else if (topics[x] === 'CSS') {
+    console.log("CSS!");
+    } else if (topics[x] === 'Git') {
+    console.log("Git!");
+    } else if (topics[x] === 'JavaScript') {
+    console.log("JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
+}
